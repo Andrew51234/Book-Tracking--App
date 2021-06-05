4d491f81fa60ea9ef68b596ca70cb2503efef582
Andrew Hany <andrewdodz51234@gmail.com>
2021-06-05 21:43:50 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,28 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './Components/App'
+import './Styles/index.css'
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-ReactDOM.render(<App/>, document.getElementById('root'))
-=======
-=======
->>>>>>> parent of 29ebb10 (first commit)
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-<<<<<<< HEAD
->>>>>>> parent of 29ebb10 (first commit)
-=======
->>>>>>> parent of 29ebb10 (first commit)
+ReactDOM.render(<App />, document.getElementById('root'))
